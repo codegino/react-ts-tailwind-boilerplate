306879d46dcc331c39daf34420cef1ffed0559b2
carlogino <carlogino@ntucenterprise.sg>
2022-01-24 09:59:49 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h3 className="text-red-800">react-typescript-tailwind boilerplate!</h3>
+    </div>
+  );
+};
+
+export default App;
